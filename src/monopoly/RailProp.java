d6d6a41443179f43104e102b3e655defa446321b
YehchanP <37120771+YehchanP@users.noreply.github.com>
2019-01-14 13:20:56 -0500

    Costs and things

---
+++ src/monopoly/RailProp.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package monopoly;
+
+/**
+ *
+ * @author 324745561
+ */
+public class RailProp extends Properties{
+
+    public RailProp(String name, int position, int cost) {
+        super(name, position, cost);
+    }
+
+    @Override
+    void event(Player p) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
